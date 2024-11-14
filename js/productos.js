47982c7212aeec8ee61747db31d7df96e3fac237
TrinidadSchek <147008723+tri1989@users.noreply.github.com>
2024-11-13 22:18:37 -0300

    Add files via upload

---
+++ js/productos.js
@@ -0,0 +1,38 @@
+const productos = [
+    {
+        nombre: 'Pink Forest',
+        precio: 5000,
+        img: 'img/productos/te1_greenForest.jpg',
+        descripcion: '100gr de Hebras de Yunnan, con arándanos rojos y pétalos de rosas.'
+    },
+    {
+        nombre: 'Honey Chai',
+        precio: 8900,
+        img: 'img/productos/te2_honeyChai.jpg',
+        descripcion: '100gr de té Chai, con canela, jengibre, anís, clavo de olor, cardamomo, pimienta y miel.'
+    },
+    {
+        nombre: 'Stawberry Fields',
+        precio: 6000,
+        img: 'img/productos/te3_StrawberryFields.jpg',
+        descripcion: '100gr de té blanco, con frutillas, láminas de coco, trocitos de mango y jengibre.'
+    },
+    {
+        nombre: 'Green Matcha',
+        precio: 10000,
+        img: 'img/productos/te4_GreenMatcha.jpg',
+        descripcion: '100gr de té verde, con matcha japonés y hierbabuena con sabor fresco y final dulce.'
+    },
+    {
+        nombre: 'Lemon Flower',
+        precio: 5900,
+        img: 'img/productos/te5_LemonFlower.jpg',
+        descripcion: '100gr de Hojas de Rooibos, con flores de jazmín, lemongrass y limón.'
+    },
+    {
+        nombre: 'Bs As Breakfast',
+        precio: 6500,
+        img: 'img/productos/te6_BsAsBreakfast.jpg',
+        descripcion: '100gr de té nero con cubos de butter toffee, trocitos de vainilla y avellanas.'
+    }
+]
